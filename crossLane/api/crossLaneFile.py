7b7b4e8c4c3a837e8b4353c8ec3c5cf4b1a4bc91
adrien-c-lopez <adrien.lopez@polytechnique.edu>
2021-07-29 17:16:08 +0200

    crossLane

--- crossLane/api/crossLaneFile.py
+++ crossLane/api/crossLaneFile.py
@@ -7,6 +7,8 @@ import rospkg
 import yaml
 
 NB_PROCESS = 1
+OPUS_START = 1
+SERIAL_TO_UPDATE = ''
 
 def run(serial, params, uuid) :
 
@@ -29,10 +31,11 @@ def run(serial, params, uuid) :
         opus = scenar[0]
         angle = scenar[1]/180*np.pi #degrees ro radians
         u_d = scenar[2]*0.514444    #knots to m/s
-        nOb = 2*scenar[3] #per lane to total
-        rlw = scenar[4]*1852. #nautical miles to m
-        llw = scenar[5]*1852. #nautical miles to m
-        ld = scenar[6]*1852. #natical miles to m
+        rld = scenar[3]             #right lane obstacle density in 1/m²
+        lld = scenar[4]             #left lane obstacle density in 1/m²
+        rlw = scenar[5]*1852.       #nautical miles to m
+        llw = scenar[6]*1852.       #nautical miles to m
+        ld = scenar[7]*1852.        #natical miles to m
         gp = 0
 
         # ASV parameters
@@ -44,12 +47,13 @@ def run(serial, params, uuid) :
 
         input = f"{rospack.get_path('asv_system')}/input/{serial}.txt"
         f = open(input,'a')
-        f.write(f'{opus}    {angle}    {u_d}    {nOb}    {rlw}    {llw}    {ld}   {gp}\n')
+        f.write(f'{opus}    {angle}    {u_d}    {rld}    {lld}    {rlw}    {llw}    {ld}   {gp}\n')
         f.close()
 
         # Creation of the launch files
-        cli_args1 = ['asv_system', 'crossLane.launch',
-                    f'nOb:={nOb}',
+        cli_args1 = ['crossLane', 'crossLane.launch',
+                     f'rld:={rld}',
+                     f'lld:={lld}',
                      f'rlw:={rlw}',
                      f'llw:={llw}',
                      f'ld:={ld}',
@@ -60,7 +64,7 @@ def run(serial, params, uuid) :
                      f'use_vo:={True}',
                      f'rviz:=False',
                      f'opus:={opus}',
-                     f'output_file:=$(find asv_system)/output/{serial}.txt',
+                     f'output_file:=$(find crossLane)/output/{serial}.txt',
                      f'pos_end_waypoint:={waypoints_asv[-1]}']
         roslaunch_file1 = roslaunch.rlutil.resolve_launch_arguments(cli_args1)[0]
         roslaunch_args1 = cli_args1[2:]
@@ -72,22 +76,25 @@ def run(serial, params, uuid) :
 
 
 if __name__ == "__main__":
+    rospack = rospkg.RosPack()
 
-    yaml_file = open("config/param/crossLane.yaml", 'r')
+    yaml_file = open(f"{rospack.get_path('crossLane')}/config/param/crossLane.yaml", 'r')
     yaml_content = yaml.safe_load(yaml_file)
 
     # UUID
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
     roslaunch.configure_logging(uuid)
     # Output parameters
-    now = datetime.datetime.now()
-    serial = now.strftime("%Y%m%d%H%M%S")[2:]
+    if len(SERIAL_TO_UPDATE) == 0: 
+        now = datetime.datetime.now()
+        serial = now.strftime("%Y%m%d%H%M%S")[2:]
+    else:
+        serial = SERIAL_TO_UPDATE
 
     # Write Input
-    rospack = rospkg.RosPack()
-    input = f"{rospack.get_path('asv_system')}/input/{serial}.txt"
+    input = f"{rospack.get_path('crossLane')}/input/{serial}.txt"
     f = open(input,'a')
-    f.write(f'OPUS    ANGLE    U_D    N_OB    RLW    LLW    LD   GROUP\n')
+    f.write(f'OPUS    ANGLE    U_D    RLD    LLD    RLW    LLW    LD   GROUP\n')
     f.close()
 
     params = []
@@ -95,22 +102,24 @@ if __name__ == "__main__":
     try:
         for angle in yaml_content['angle']:
             for u_d in yaml_content['u_d']:
-                for nOb in yaml_content['nOb']:
-                    for rlw in yaml_content['rlw']:
-                        for llw in yaml_content['llw']:
-                            for ld in yaml_content['ld']:
-                                params.append([opus, angle, u_d, nOb, rlw, llw, ld])
-                                opus += 1
-                                if len(params) == NB_PROCESS:
-                                    #try:
-                                    run(serial, params, uuid)
-                                    params = []
-                                    # except:
-                                    #     print("Unexpected error:", sys.exc_info()[0])
-                                    #     output = f"{rospack.get_path('asv_system')}/output/{serial}.txt"
-                                    #     g = open(input,'a')
-                                    #     g.write(f'{opus+1} nan nan nan nan nan nan nan nan nan nan nan nan nan\n')
-                                    #     g.close()
-                                    #     params = []
+                for rld in yaml_content['rld']:
+                    for lld in yaml_content['rld']:
+                        for rlw in yaml_content['rlw']:
+                            for llw in yaml_content['llw']:
+                                for ld in yaml_content['ld']:
+                                    if opus > OPUS_START:
+                                        params.append([opus, angle, u_d, rld, lld, rlw, llw, ld])
+                                    opus += 1
+                                    if len(params) == NB_PROCESS:
+                                        #try:
+                                        run(serial, params, uuid)
+                                        params = []
+                                        # except:
+                                        #     print("Unexpected error:", sys.exc_info()[0])
+                                        #     output = f"{rospack.get_path('asv_system')}/output/{serial}.txt"
+                                        #     g = open(input,'a')
+                                        #     g.write(f'{opus+1} nan nan nan nan nan nan nan nan nan nan nan nan nan\n')
+                                        #     g.close()
+                                        #     params = []
     except KeyboardInterrupt:
         pass
